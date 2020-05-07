0a8ae23f03b6984565dea8562cfb321516742dc1
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-05-07 12:16:29 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,6 +12,27 @@ variable "instance_type" {
   default     = "t2.micro"
 }
 
-variable "ami" {
-  description = "Base AMI to launch the instances"
+variable "region" {
+description = "AWS region for hosting our your network"
+default = "us-east-2"
+}
+
+variable "vpc_cidr" {
+description = "CIDR for VPC"
+default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+description = "CIDR for public subnet"
+default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+description = "CIDR for private subnet"
+default     = "10.0.2.0/24"
+}
+
+variable "amis" {
+description = "Base AMI to launch the instances"
+default = "ami-be7753db"
 }
