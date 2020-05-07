cecf61a3533f0d70737166924f13e7e08e8dff89
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-05-07 14:45:40 -0500

    Delete private.tf

--- private.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "private-subnet" {
-  vpc_id = "${aws_vpc.default.id}"
-  cidr_block = "${var.private_subnet_cidr}"
-  availability_zone = "us-east-2b"
-
-  tags = {
-  Name = "Linoxide Private Subnet"
-}
-}
