ab43134d663eb3444560aed132af1a956c01fe79
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-05-07 13:30:58 -0500

    Update dbinstance.tf

--- dbinstance.tf
+++ dbinstance.tf
@@ -3,7 +3,7 @@ ami = "var.amis"
 availability_zone = "us-east-2b"
 instance_type = "t2.micro"
 key_name = var.key_name
-vpc_security_group_ids = ["${aws_security_group.db.id}"] 
+vpc_security_group_ids = [aws_security_group.db.id] 
 subnet_id = "${aws_subnet.private-subnet.id}"
 source_dest_check = false
 
