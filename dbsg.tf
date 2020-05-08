ee62328740742b602731aa3f292a2c637f1f6ea8
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-05-08 14:32:12 -0500

    Update dbsg.tf

--- dbsg.tf
+++ dbsg.tf
@@ -6,20 +6,20 @@ ingress {
 from_port = 3306
 to_port = 3306
 protocol = "tcp"
-security_groups = [aws_security_group.web.id] 
+security_groups = ["${aws_security_group.web.id}"] 
 }
 
 ingress {
 from_port = 22
 to_port = 22
 protocol = "tcp"
-cidr_blocks = [var.vpc_cidr] 
+cidr_blocks = ["${var.vpc_cidr}"] 
 }
 ingress {
 from_port = -1
 to_port = -1
 protocol = "icmp"
-cidr_blocks = [var.vpc_cidr] 
+cidr_blocks = ["${var.vpc_cidr}"] 
 }
 
 egress {
@@ -35,7 +35,7 @@ protocol = "tcp"
 cidr_blocks = ["0.0.0.0/0"] 
 }
 
-vpc_id = aws_vpc.default.id
+vpc_id = "${aws_vpc.default.id}"
 
 tags = {
 Name = "DBServerSG"
