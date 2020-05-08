ab418b825c9dad02ef34a34a5ab49d387e475dc0
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-05-08 12:55:48 -0500

    Create natinstance.tf

---
+++ natinstance.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "nat" {
+ami = "ami-be7753db" # this is a special ami preconfigured to do NAT
+availability_zone = "us-east-2b"
+instance_type = "t2.micro"
+key_name = "${var.key_name}"
+vpc_security_group_ids = ["${aws_security_group.nat.id}"] 
+subnet_id = "${aws_subnet.public-subnet.id}"
+associate_public_ip_address = true
+source_dest_check = false
+
+tags = {
+Name = "NAT instance"
+}
+}
