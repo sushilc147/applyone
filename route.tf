ab8960ecfeb23c61b6bde8e4554a44c7b3d3aa84
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-05-08 13:13:37 -0500

    Update route.tf

--- route.tf
+++ route.tf
@@ -15,21 +15,3 @@ resource "aws_route_table_association" "web-public-rt" {
   subnet_id = "${aws_subnet.public-subnet.id}"
   route_table_id = "${aws_route_table.web-public-rt.id}"
 }
-
-resource "aws_route_table" "web-private-rt" {
-  vpc_id = "${aws_vpc.default.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    instance_id = "${aws_instance.nat.id}"
-  }
-
-  tags = {
-  Name = "Linoxide Private RT"
-  }
-}
-
-resource "aws_route_table_association" "web-private-rt" {
-  subnet_id = "${aws_subnet.private-subnet.id}"
-  route_table_id = "${aws_route_table.web-private-rt.id}"
-}
