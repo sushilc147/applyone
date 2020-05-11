05cfe73caf3425fe8dbd455951789b9e78ee6ddd
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-05-11 13:46:17 -0500

    Update webserver.tf

--- webserver.tf
+++ webserver.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web-1" {
-ami = var.amis
+ami = "${var.amis}"
 availability_zone = "us-east-2b"
 instance_type = "t2.micro"
-key_name = var.key_name
+key_name = "${var.key_name}"
 vpc_security_group_ids = ["${aws_security_group.web.id}"] 
 subnet_id = "${aws_subnet.public-subnet.id}"
 associate_public_ip_address = true
