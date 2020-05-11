1d0770b800c8a910cb1d47145f9508c99667f715
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-05-11 13:48:55 -0500

    Update webserver.tf

--- webserver.tf
+++ webserver.tf
@@ -4,7 +4,7 @@ availability_zone = "us-east-2b"
 instance_type = "t2.micro"
 key_name = "${var.key_name}"
 vpc_security_group_ids = ["${aws_security_group.web.id}"] 
-subnet_id = "${aws_subnet.public-subnet.id}"
+subnet_id = aws_subnet.public-subnet.id
 associate_public_ip_address = true
 source_dest_check = false
 
