06a633f31e7be7ee2c26b8189f6c142c893babfd
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-05-11 13:57:06 -0500

    Update dbinstance.tf

--- dbinstance.tf
+++ dbinstance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "db-1" {
-ami = "ami-be7753db
+ami = "ami-be7753db"
 availability_zone = "us-east-2b"
 instance_type = "t2.micro"
 vpc_security_group_ids = ["${aws_security_group.db.id}"] 
