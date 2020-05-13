5a115411e6e19e3c52fbed9dd9026e6ad7e7de88
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-05-13 14:43:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "adk-state"
+  bucket = "jpt-state"
    Enable versioning so we can see the full revision history of our state files
   versioning {
     enabled = true
@@ -15,7 +15,7 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-locks"
+  name         = "state-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
