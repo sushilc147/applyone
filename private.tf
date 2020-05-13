337604aad88750d21e6c33f2529a2ac73731b7ce
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-05-13 14:23:49 -0500

    Update private.tf

--- private.tf
+++ private.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "private-subnet" {
+/*resource "aws_subnet" "private-subnet" {
   vpc_id = "${aws_vpc.default.id}"
   cidr_block = "${var.private_subnet_cidr}"
   availability_zone = "us-east-2b"
@@ -6,4 +6,4 @@ resource "aws_subnet" "private-subnet" {
   tags = {
   Name = "Linoxide Private Subnet"
 }
-}
+}*/
