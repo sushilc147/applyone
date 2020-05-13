9d492c04cbc1a2c75d3471d9f4e36772f2f57ea6
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-05-12 23:02:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state"
+  bucket = "adk-state"
   # Enable versioning so we can see the full revision history of our state files
   versioning {
     enabled = true
