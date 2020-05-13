90579e084f6c7a80951990423da84e3442f82c2f
adkbhoje <34731103+adkbhoje@users.noreply.github.com>
2020-05-13 14:24:13 -0500

    Update public.tf

--- public.tf
+++ public.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "public-subnet" {
+/*resource "aws_subnet" "public-subnet" {
   vpc_id = "${aws_vpc.default.id}"
   cidr_block = "${var.public_subnet_cidr}"
   availability_zone = "us-east-2b"
@@ -6,4 +6,4 @@ resource "aws_subnet" "public-subnet" {
   tags = {
   Name = "Linoxide Public Subnet"
 }
-}
+}*/
